aba559b8f7807f178c349bbb71cb955060926593
BugHunter-X <askzenai@gmail.com>
2024-12-27 11:38:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,11 @@
+def function(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero"
+
+result = function(10, 0)
+print(result) 
+
+result = function(10, 2)
+print(result) 
